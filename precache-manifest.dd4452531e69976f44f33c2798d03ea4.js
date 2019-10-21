1c087861f8ca59fbada26c5da951e7454e22cb29
yaser1rifai <yaser1rifai@gmail.com>
2019-10-21 22:51:52 +0200

    Updates

---
+++ precache-manifest.dd4452531e69976f44f33c2798d03ea4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d2dfc7e5335264d72f7ff818e0117c",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "a09c3c469f28fce6b3da",
+    "url": "/todoapp/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "470a87ce93f3a9f4f5cb",
+    "url": "/todoapp/static/js/2.71d46483.chunk.js"
+  },
+  {
+    "revision": "a09c3c469f28fce6b3da",
+    "url": "/todoapp/static/js/main.f60ea84d.chunk.js"
+  },
+  {
+    "revision": "9da8c2afb69611eac807",
+    "url": "/todoapp/static/js/runtime~main.1a0ff1c8.js"
+  }
+]);
