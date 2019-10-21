f43ed4c895b3d1cac63de532656cbfdce0467503
yaser1rifai <yaser1rifai@gmail.com>
2019-10-21 22:53:21 +0200

    todoapp react

---
+++ src/AddTodo.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export default class AddTodo extends Component {
+    state = {
+        content: ''
+    }
+    handelchange = (e) => {
+        this.setState({
+            content: e.target.value
+        })
+    }
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.addTodo(this.state)
+        this.setState({content:''})
+
+    }
+
+  x
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Add new todo:</label>
+                    <input type="text" onChange={this.handelchange} value={this.state.content} />
+
+                </form>
+            </div>
+        )
+    }
+}
